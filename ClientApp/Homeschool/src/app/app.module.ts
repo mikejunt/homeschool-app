c2ef4e74b2b7757d83a158edfa761736a6768058
mikejunt <mikejunt@gmail.com>
2020-08-20 17:16:17 -0500

    adds material module and configures appmodule

--- ClientApp/Homeschool/src/app/app.module.ts
+++ ClientApp/Homeschool/src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AdminLandingComponent } from './admin/admin-landing/admin-landing.component';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ErrorComponent } from './shared/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    AdminLandingComponent
+    AdminLandingComponent,
+    ToolbarComponent,
+    FooterComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule,
     StoreModule.forRoot({}, {})
   ],
   providers: [],
