17f32e643951dc69ccf2c3a70003c4f2553526a0
mikejunt <mikejunt@gmail.com>
2020-08-22 14:46:19 -0500

    sets up basic toolbar component

--- ClientApp/Homeschool/src/app/shared/toolbar/toolbar.component.ts
+++ ClientApp/Homeschool/src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { RootState } from '../../store';
+import * as Selectors from '../../store/selectors';
+import * as Actions from '../../store/actions';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/interfaces/user.interface';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +12,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  user$: Observable<User>
+  user: User
 
-  constructor() { }
+  constructor(private store: Store<RootState>) {
+    this.user$ = this.store.select(Selectors.getUserInfo) }
 
   ngOnInit(): void {
+    this.user$.subscribe(userstate => {
+      console.log("Toolbar Component:", userstate)
+      this.user = userstate})
+  }
+
+  logOutUser() {
+    this.store.dispatch(Actions.clearUser())
   }
 
 }
