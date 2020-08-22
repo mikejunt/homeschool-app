c0a372758b7098711d9b1dccc8e0904242a8ae74
mikejunt <mikejunt@gmail.com>
2020-08-22 17:27:12 -0500

    connects auth to user service

---
+++ ClientApp/src/app/interfaces/auth0user.interface.ts
@@ -0,0 +1,12 @@
+export interface Auth0UserProfile {
+    given_name: string
+    family_name: string
+    nickname: string
+    name: string
+    picture: string
+    locale: string
+    updated_at: string
+    email: string
+    email_verified: boolean
+    sub: string
+}
