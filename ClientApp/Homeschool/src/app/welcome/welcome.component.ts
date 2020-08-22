781f42506d3168c5a3082cce07b1a752d6fda184
mikejunt <mikejunt@gmail.com>
2020-08-22 14:48:57 -0500

    adds login button to welcome componnet

--- ClientApp/Homeschool/src/app/welcome/welcome.component.ts
+++ ClientApp/Homeschool/src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service'
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    console.log("login function called")
   }
 
 }
