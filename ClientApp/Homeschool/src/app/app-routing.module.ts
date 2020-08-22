846951471d407986add1fcc63584863b0cddee0f
mikejunt <mikejunt@gmail.com>
2020-08-22 15:37:56 -0500

    applies guard to route

--- ClientApp/Homeschool/src/app/app-routing.module.ts
+++ ClientApp/Homeschool/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard'
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AdminLandingComponent } from './admin/admin-landing/admin-landing.component';
 import { ErrorComponent } from './shared/error/error.component';
 
+
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'admin', component: AdminLandingComponent },
+  { path: 'admin', component: AdminLandingComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', component: ErrorComponent }
 ];
