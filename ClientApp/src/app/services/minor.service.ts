64431d203837cc9c7a0f6824c1216c7c81ff901c
mikejunt <mikejunt@gmail.com>
2020-08-22 20:26:07 -0500

    fills out minor data functions

--- ClientApp/src/app/services/minor.service.ts
+++ ClientApp/src/app/services/minor.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { RootState } from '../store';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user.interface';
+import * as Actions from '../store/actions';
+import * as Selectors from '../store/selectors';
+import { UserMembership } from '../interfaces/user-membership.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MinorService {
 
-  constructor() { }
+  constructor(private store: Store<RootState>, private http: HttpClient) { }
+
+  getUsersMinors(email: string) {
+    this.http.get(`https://hsappapi.azurewebsites.net/api/users/minors/${email}`).subscribe((minors: User[]) => {
+    this.store.dispatch(Actions.setMinorInfo({minors: minors}))
+    let mids: number[] = []
+    minors.forEach(obj => mids.push(obj.id))
+    console.log("Minor Ids:", mids)
+    let querystring = "https://hsappapi.azurewebsites.net/api/family/minors?"
+    mids.forEach(val => querystring.concat(`uids=${val}&`))
+    console.log("querystring is now:", querystring)
+    this.http.get(querystring).subscribe((memberships: UserMembership[]) => {
+      this.store.dispatch(Actions.setMinorMemberships({membership: memberships}))
+    })
+    })
+  }
 }
