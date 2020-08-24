9789f5e14a001680c879d12b34bcd8a29404bc0d
mikejunt <mikejunt@gmail.com>
2020-08-24 11:29:59 -0500

    tasks base now tracks to viewed user

--- ClientApp/src/app/tasks/task-base/task-base.component.ts
+++ ClientApp/src/app/tasks/task-base/task-base.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import * as Selectors from '../../store/selectors';
+import { Store } from '@ngrx/store';
+import { RootState } from 'src/app/store';
+import { Observable, combineLatest } from 'rxjs';
+import { FamilyMember } from 'src/app/interfaces/family-member.interface';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-task-base',
@@ -6,10 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-base.component.scss']
 })
 export class TaskBaseComponent implements OnInit {
+  viewfid$: Observable<number>
+  viewuid$: Observable<number>
+  familymembers$: Observable<FamilyMember[]>
+  viewfid: number
+  viewuid: number
+  viewmember: FamilyMember
 
-  constructor() { }
+  constructor(private store: Store<RootState>) {
+    this.viewfid$ = this.store.select(Selectors.getViewedFamily)
+    this.viewuid$ = this.store.select(Selectors.getViewedUser)
+    this.familymembers$ = this.store.select(Selectors.getFamilyMembers)
+   }
 
   ngOnInit(): void {
+    combineLatest([this.viewfid$, this.viewuid$, this.familymembers$]).pipe(
+      map(([fid, uid, members]) => ({fid, uid, members}))
+  )
+  .subscribe(data => {
+      this.viewfid = data.fid
+      this.viewuid = data.uid;
+      let target = data.members.find(obj => { return obj.id === data.uid && obj.familyId === data.fid })
+      this.viewmember = {...target}
+  })
   }
 
 }
