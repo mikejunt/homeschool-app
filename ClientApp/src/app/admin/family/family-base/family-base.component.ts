11abe7e1b2a0d6681d14415ae0a253b7be920ae2
mikejunt <mikejunt@gmail.com>
2020-08-24 17:38:51 -0500

    arranges family list components, inputs, and state

--- ClientApp/src/app/admin/family/family-base/family-base.component.ts
+++ ClientApp/src/app/admin/family/family-base/family-base.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserMembership } from 'src/app/interfaces/user-membership.interface';
+import { FamilyMember } from 'src/app/interfaces/family-member.interface';
+import { Store } from '@ngrx/store';
+import { RootState } from 'src/app/store';
+import * as Selectors from '../../../store/selectors'
 
 @Component({
   selector: 'app-family-base',
@@ -6,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./family-base.component.scss']
 })
 export class FamilyBaseComponent implements OnInit {
+  @Input() displayfid: number
+  memberships$: Observable<UserMembership[]>
+  membership: UserMembership
+  familymembers$: Observable<FamilyMember[]>
+  familymembers: FamilyMember[]
 
-  constructor() { }
+  constructor(private store: Store<RootState>) {
+    this.memberships$ = this.store.select(Selectors.getUserMemberships)
+    this.familymembers$ = this.store.select(Selectors.getFamilyMembers)
+   }
 
   ngOnInit(): void {
+    this.memberships$.subscribe((state: UserMembership[]) => {
+      let filtered = [...state.filter((family: UserMembership) => family.familyId === this.displayfid)]
+      console.log("FID:", this.displayfid, "FamData:", filtered)
+      this.membership = filtered[0]
+    })
+    this.familymembers$.subscribe((state: FamilyMember[]) => {
+      let filtered = [...state.filter((member: FamilyMember) => member.familyId === this.displayfid)]
+      console.log("FID:", this.displayfid, "Members:", filtered)
+      this.familymembers = filtered
+    })
   }
 
 }
--- ClientApp/src/app/admin/family/family-list/family-list.component.ts
+++ ClientApp/src/app/admin/family/family-list/family-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FamilyMember } from 'src/app/interfaces/family-member.interface';
+import { Store } from '@ngrx/store';
+import { RootState } from 'src/app/store';
+import * as Selectors from '../../../store/selectors'
 
 @Component({
   selector: 'app-family-list',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./family-list.component.scss']
 })
 export class FamilyListComponent implements OnInit {
+  @Input() viewuid: number
+  @Input() viewfid: number
+  familymembers$: Observable<FamilyMember[]>
+  familymember: FamilyMember
 
-  constructor() { }
-
+  constructor(private store: Store<RootState>) {
+    this.familymembers$ = this.store.select(Selectors.getFamilyMembers)
+  }
   ngOnInit(): void {
+    this.familymembers$.subscribe((state: FamilyMember[]) => {
+      let filtered = state.filter((member: FamilyMember) => member.id === this.viewuid && member.id === this.viewfid)
+      console.log("UID:", this.viewuid, "FID:", this.viewfid, "Result:", filtered)
+      this.familymember = filtered[0]
+    })
   }
 
 }
