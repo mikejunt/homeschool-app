16b81faf6b5eccc87f52a633ed035010fb12cf20
mikejunt <mikejunt@gmail.com>
2020-08-25 19:28:33 -0500

    permits users to navigate to own profile to confirm

--- ClientApp/src/app/admin/family/family-list/family-list.component.ts
+++ ClientApp/src/app/admin/family/family-list/family-list.component.ts
@@ -6,6 +6,7 @@ import { RootState } from 'src/app/store';
 import * as Selectors from '../../../store/selectors';
 import * as Actions from '../../../store/actions';
 import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/interfaces/user.interface';
 
 @Component({
   selector: 'app-family-list',
@@ -18,14 +19,20 @@ export class FamilyListComponent implements OnInit {
   @Input() index: number
   familymembers$: Observable<FamilyMember[]>
   familymember: FamilyMember
+  user$: Observable<User>
+  user: User
 
   constructor(private store: Store<RootState>, private router: Router, private actr: ActivatedRoute) {
     this.familymembers$ = this.store.select(Selectors.getFamilyMembers)
+    this.user$ = this.store.select(Selectors.getUserInfo)
   }
   ngOnInit(): void {
     this.familymembers$.subscribe((state: FamilyMember[]) => {
       let filtered = state.filter((member: FamilyMember) => member.id === this.viewuid && member.familyId === this.viewfid)
       this.familymember = filtered[0]
+    })
+    this.user$.subscribe((state: User) => {
+      this.user = state
     })
   }
   navigate() {
